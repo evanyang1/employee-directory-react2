bf219aad3b93edcdd90d02431e3470bfa4472a3b
Evan Yang <evanyang@pop-os.localdomain>
2020-03-26 13:27:29 -0700

    Updates

---
+++ precache-manifest.81c24ca5799741b0be0a4f28539d2b45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b42b610be4730a021b420efcbe045ff3",
+    "url": "/employee-directory-react2/index.html"
+  },
+  {
+    "revision": "71f1e08a8088129dc9ac",
+    "url": "/employee-directory-react2/static/js/2.55beb425.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/employee-directory-react2/static/js/2.55beb425.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba3f8ea269fba569796b",
+    "url": "/employee-directory-react2/static/js/main.8fa5b034.chunk.js"
+  },
+  {
+    "revision": "3ea88e711755c7a6c4c3",
+    "url": "/employee-directory-react2/static/js/runtime-main.c27b48c7.js"
+  }
+]);
